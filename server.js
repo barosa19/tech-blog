da4b0cd4277748ba98a6069cec6bfd9fe32df478
Brosa <brunoarosa1997@gmail.com>
2023-04-28 13:36:05 -0400

    connected to sequelize

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 //Dependencies
-const express = require('express')
-const path = require('path')
-const routes = require('./controller')
-const exphbs = require('express-handlebars')
-const hbs = exphbs.create({})
+const express = require("express");
+const path = require("path");
+//const routes = require("./controller");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+const sequelize = require("./config/connection");
 
 //Sets up the Express App
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 //Sets Handlebars
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars')
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 //Middleware
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.static(path.join(__dirname,'public')))
-app.use(routes)
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, "public")));
+//app.use(routes);
 
 // Starts the server
-app.listen(PORT, () => console.log('Now listening'))
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
