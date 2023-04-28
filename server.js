91a2cf911d389d27b0e656fc3530db9d2a8d7a2f
Brosa <brunoarosa1997@gmail.com>
2023-04-28 13:09:52 -0400

    setup express on server.js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+//Dependencies
 const express = require('express')
+const path = require('path')
+const routes = require('./controller')
 const exphbs = require('express-handlebars')
+const hbs = exphbs.create({})
 
+//Sets up the Express App
 const app = express()
 const PORT = process.env.PORT || 3001
 
-const hbs = exphbs.create({})
-
+//Sets Handlebars
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static(path.join(__dirname,'public')))
+app.use(routes)
 
+// Starts the server
 app.listen(PORT, () => console.log('Now listening'))
